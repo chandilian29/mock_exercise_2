c5ac0d425beb248480f4de05dca38f7d4f0f5815
chandilian29 <chandilian29@gmail.com>
2020-11-05 05:47:41 -0800

    Update mock.tsx

--- src/mock.tsx
+++ src/mock.tsx
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 const idOne = uuidv4();
 const idTwo = uuidv4();
@@ -28,7 +28,40 @@ export const getUsers = () => {
 export const createUser = (data: any) => {
     //we dont care about validation
     const id = uuidv4();
-    const newUser = {id, ...data};
+    const newUser = {
+        id: id,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        role: data.role,
+        enrollDate: data.enrollDate
+    };
     users = {...users, [id]: newUser}; //mock services should manipulate the mock db ("users" object)
     return newUser;
+}
+
+export const updateUserValue = (data: any) => {
+    //we dont care about validation
+    getUsers().forEach(user => {
+        if (user.id === data.id) {
+            user.firstName = data.firstName;
+            user.lastName = data.lastName;
+            user.role = data.role;
+            user.enrollDate = data.enrollDate;
+        }
+    })
+    //mock services should manipulate the mock db ("users" object)
+    return users;
+}
+
+export const deleteUserValue = (data: any) => {
+    //we dont care about validation
+    let tempUser= {};
+    getUsers().forEach(user => {
+        if (user.id !== data.id) {
+            tempUser = {...tempUser, [user.id]:user};
+        }
+    })
+    users = tempUser;
+    //mock services should manipulate the mock db ("users" object)
+    return users;
 }
